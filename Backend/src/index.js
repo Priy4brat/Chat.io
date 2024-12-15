d46e9cf1bf3412146e41054661a626080bcc49c7
Priyabrata Das <141203796+Priy4brat@users.noreply.github.com>
2024-12-15 10:53:01 +0530

    project finished

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,32 +1,40 @@
-import  express  from "express";
-import authRoutes from './routes/auth.route.js'
-import messageRoutes from './routes/message.route.js'
-import dotenv from "dotenv"
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
-import cookieParser from "cookie-parser"
-import cors from "cors"
-import { app , server} from "./lib/socket.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { app, server } from "./lib/socket.js";
 
+import path from "path";
 
-dotenv.config() // it will load the .env file into the process.env object
-const PORT = process.env.PORT || 5002
+dotenv.config(); // it will load the .env file into the process.env object
+const PORT = process.env.PORT || 5002;
+const __dirname = path.resolve();
 
-app.use(cors({
-
+app.use(
+  cors({
     origin: "http://localhost:5173",
     credentials: true,
-}))
- 
-
-app.use(express.json({ limit: '10mb' })); 
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+  })
+);
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 // app.use(express.json()) // it will parse the incoming requests with JSON data
-app.use(cookieParser()) // it will parse the incoming requests with cookies
-app.use("/api/auth", authRoutes) // it will trigger to the routes folder 
-app.use("/api/messages", messageRoutes)
+app.use(cookieParser()); // it will parse the incoming requests with cookies
+app.use("/api/auth", authRoutes); // it will trigger to the routes folder
+app.use("/api/messages", messageRoutes);
 
-server.listen(PORT,()=>{
-    console.log(`server is running at http://localhost:${PORT}`); 
-    connectDB();
-    
-})
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"))
+  });
+}
+
+server.listen(PORT, () => {
+  console.log(`server is running at http://localhost:${PORT}`);
+  connectDB();
+});
